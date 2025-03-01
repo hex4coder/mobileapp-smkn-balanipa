9b1e016e007972eb71a4f5bd20afa4d64ebdc76a
hex4coder <the.programmer.luyo@gmail.com>
2025-03-01 08:35:45 +0800

    realease app v1.0

--- lib/configs/server.dart
+++ lib/configs/server.dart
@@ -4,7 +4,8 @@ import 'package:flutter/material.dart';
 class ServerConfig {
   // base url
   // static String kServerBaseAPI = "https://ecom-api.smknbalanipa.sch.id";
-  static String kServerBaseAPI = "http://10.0.2.2:3001";
+//  static String kServerBaseAPI = "http://10.0.2.2:3001";
+  static String kServerBaseAPI = "http://89.116.20.48:3000";
 
   // default network image for no image
   static String kNoImage =
